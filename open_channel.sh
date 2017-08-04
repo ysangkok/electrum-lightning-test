73a6149614c7527967d7d54ae98fd6548ac265bb
Janus <ysangkok@gmail.com>
2017-08-04 21:45:14 +0200

    update to latest lnd

--- open_channel.sh
+++ open_channel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 VANILLAKEY=$(./connect_lnd_nodes.sh)
-docker exec -it electrumlightningtest_eleclnd_1 /go/bin/lncli openchannel --node_key=$VANILLAKEY --num_confs=1 --local_amt=10000
+docker exec -it electrumlightningtest_eleclnd_1 /go/bin/lncli openchannel --node_key=$VANILLAKEY --local_amt=10000
