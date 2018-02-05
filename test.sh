f7e674bfd007e095e0a72fcca23407c7bbd3307a
Janus <ysangkok@gmail.com>
2018-02-05 14:42:40 +0100

    test.sh: use venv

--- test.sh
+++ test.sh
@@ -3,16 +3,24 @@ cd electrum-lightning-hub
 PYTHONPATH=../electrum/lib/ln screen -S lightning-hub -d -m python3.6 repeater_and_rpc.py
 screen -ls
 sleep 10
-git clone https://github.com/spesmilo/electrum.git ../electrum
-cd ../electrum
-git checkout lightning
+if [ ! -d ../electrum ]; then
+  git clone https://github.com/spesmilo/electrum.git ../electrum
+  cd ../electrum
+  git checkout lightning
+else
+	cd ../electrum
+fi
+if [ ! -d ../venv ]; then
+	python3.6 -m venv ../venv
+fi
+../venv/bin/pip install -r contrib/deterministic-build/requirements.txt
 rm -rf ~/.electrum/testnet
 PYTHONPATH=lib/ln ../create.expect
-PYTHONPATH=lib/ln python3.6 ./electrum --testnet daemon start
-PYTHONPATH=lib/ln python3.6 ./electrum --testnet daemon load_wallet
+PYTHONPATH=lib/ln ../venv/bin/pip/python ./electrum --testnet daemon start
+PYTHONPATH=lib/ln ../venv/bin/pip/python ./electrum --testnet daemon load_wallet
 sleep 5
 while true; do
-  OUT="$(PYTHONPATH=lib/ln python3.6 ./electrum --testnet lightning getinfo)"
+  OUT="$(PYTHONPATH=lib/ln ../venv/bin/pip/python ./electrum --testnet lightning getinfo)"
   CODE="$(echo $OUT | jq .returncode)"
   if [[ $CODE == "null" ]]; then
     # returncode is only there on error (see lncli_endpoint.py)
@@ -21,5 +29,5 @@ while true; do
   fi
   echo "$OUT"
 done
-PYTHONPATH=lib/ln python3.6 ./electrum --testnet daemon stop
+PYTHONPATH=lib/ln ../venv/bin/pip/python ./electrum --testnet daemon stop
 screen -X -S lightning-hub quit
