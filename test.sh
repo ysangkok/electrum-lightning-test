e3968edf794536c2fee6d75942721b55317de4bd
Janus <ysangkok@gmail.com>
2018-02-21 16:25:03 +0100

    test.sh: wait longer before checking if channel was fully opened

--- test.sh
+++ test.sh
@@ -122,7 +122,7 @@ done
 echo $NODE2PUBK 100000 | python3 -c 'import json, sys; print(json.dumps(sys.stdin.read().rstrip().split(" ")))' | bash -c "time ../venv/bin/python electrum --testnet lightning openchannel -D $ELECDIR1 --lightningargs -"
 PYTHONPATH=lib/ln ../venv/bin/python electrum --testnet lightning listchannels -D $ELECDIR1
 #screen -X -S lightning-hub quit
-sleep 600
+sleep 1200
 for ELECDIR in $ELECDIR1 $ELECDIR2; do
   PYTHONPATH=lib/ln ../venv/bin/python ./electrum --testnet daemon status -D $ELECDIR
 done
