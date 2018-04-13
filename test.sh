5ca07254c5a46a6e1d11982be4ff1db9ac02d45a
Janus <ysangkok@gmail.com>
2018-04-13 21:23:48 +0200

    test.sh: wait for confirmation of 0.003 lightning wallet funding

--- test.sh
+++ test.sh
@@ -106,12 +106,12 @@ done
 ../venv/bin/python electrum --testnet daemon status # TODO check if server_height == local_height
 ../venv/bin/python electrum --testnet getbalance
 ../venv/bin/python electrum --testnet listaddresses
-../venv/bin/python electrum --testnet payto $NODE1ADDR 0.10 | ../venv/bin/python electrum --testnet broadcast -
+../venv/bin/python electrum --testnet payto $NODE1ADDR 0.003 | ../venv/bin/python electrum --testnet broadcast -
 sleep 1
 ../venv/bin/python electrum --testnet daemon stop
 cd -
 
-sleep 60
+sleep 1600
 
 for ELECDIR in $ELECDIR1 $ELECDIR2; do
   PYTHONPATH=lib/ln ../venv/bin/python ./electrum --testnet daemon status -D $ELECDIR
